e0a5301e28ae7f619b15ee82846c215b8034342a
Putcho <g.putcho@gmail.com>
2016-10-19 17:15:30 +0900

    2016-10-19

--- src/net/putcho/bukkit/signshell/SignShell.java
+++ src/net/putcho/bukkit/signshell/SignShell.java
@@ -8,6 +8,36 @@ public class SignShell {
 
 	}
 
+	static final String[] reservedWords = {
+			"abstract",
+			"boolean", "break", "byte",
+			"case", "catch", "char", "class", "continue",
+			"default", "do",
+			"else", "extends",
+			"false", "final", "float", "for",
+			"implements", "import", "int", "interface",
+			"new", "null",
+			"package", "private", "protected", "public",
+			"return",
+			"short", "static", "super", "switch", "synchronized",
+			"this", "throws", "try", "true",
+			"void",
+			"while",
+	};
+
+	static final String[] symbols = {
+			"+", "-", "*", "/", "=", "&", "|",
+			"++", "+=", "--", "-=", "**", "*=", "/=", "==", "!=",
+			"&&", "||", "&=", "|=",
+	};
+
+	static final String[] chars = {
+			"{", "}",
+			"[", "}",
+			"(", ")",
+			",", "."
+	};
+
 	private char ln;
 
 	private char[] src;
@@ -57,20 +87,52 @@ public class SignShell {
 				if(read()){
 					if(read == '/'){
 						while(read()){
-							if(read == ln){
-								add(word.substring(0, word.length() - 1), Type.COMMENT);
-								continue roop;
-							}
+							if(read == ln)break;
 						}
-						add(word, Type.COMMENT);
-						break roop;
+						this.resetWord();
+						continue roop;
 					}else if(read == '*'){
-
+						while(read()){
+							if(word.matches("/\\*.*\\*/"))break;
+						}
+						this.resetWord();
+						continue roop;
 					}
 				}
 			}
-			if(Character.isWhitespace(read)){//空白文字無視
 
+			int cha;
+			if(isSymbol()){
+				if(read()){
+					for(String symbol: symbols){
+						if(word.equals(symbol)){
+							this.add(word, Type.SYMBOL);
+							continue roop;
+						}
+					}
+					this.add(word.substring(0, word.length() - 1), Type.SYMBOL);
+					word = String.valueOf(read);
+				}else{
+					break roop;
+				}
+			}
+			if((cha = getChartype()) < chars.length){
+				if(word.length() > 1){
+					this.add(word.substring(0, word.length() - 1));
+				}
+				if(cha < 6){
+					this.add(String.valueOf(read), Type.BRACKET);
+				}else if(cha < 7){
+					this.add(String.valueOf(read), Type.COMMA);
+				}else if(cha < 8){
+					this.add(String.valueOf(read), Type.PERIOD);
+				}
+			}else if(Character.isWhitespace(read)){
+				if(word.length() > 1){
+					this.add(word.substring(0, word.length() - 1));
+				}else{
+					this.resetWord();
+				}
 			}
 		}
 	}
@@ -81,11 +143,31 @@ public class SignShell {
 
 	void add(String word, Type type){
 		words.add(new Hoge(word, type));
+		this.resetWord();
+	}
+
+	void resetWord(){
 		this.word = "";
 	}
 
 	Type getType(String word){
 		return Type.NAME;
+	}
+
+	boolean isSymbol(){
+		for(int i = 0; i < 7; i++){
+			if(String.valueOf(read).equals(symbols[i]))return true;
+		}
+		return false;
+	}
+
+	int getChartype(){
+		int cnt = 0;
+		while(cnt < chars.length){
+			if(chars[cnt].charAt(0) == read)break;
+			cnt++;
+		}
+		return cnt;
 	}
 }
 
@@ -99,8 +181,10 @@ class Hoge{
 }
 
 enum Type{
-	COMMENT,
 	SYMBOL,
 	RESERVED,
+	BRACKET,
+	COMMA,
+	PERIOD,
 	NAME,
 }
